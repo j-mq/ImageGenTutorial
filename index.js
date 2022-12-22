7c9d61e7a09c061d55bfdaf55485d8c4ef22dc9a
Jacobo <jacobo@fixel.co.jp>
2022-12-22 17:46:07 +0900

    setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(123);
+const express = require('express');
+const env = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
