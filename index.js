18a12deac7e2306cbc366ffdfe444bde03dc01ba
Jacobo <jacobo@fixel.co.jp>
2022-12-27 16:45:45 +0900

    Backend done

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/openai', require('./routes/openaiRoutes'));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
